b111d5564eb9b186ce10ce75faa1682367fd3bb4
Ivy Halpin <yoyofoe1@gmail.com>
2023-03-30 13:23:41 -0400

    Retry last commit

--- java/src/main/java/com/techelevator/application/VendingMachine.java
+++ java/src/main/java/com/techelevator/application/VendingMachine.java
@@ -1,11 +1,21 @@
 package com.techelevator.application;
 
+import com.techelevator.models.*;
 import com.techelevator.ui.UserInput;
 import com.techelevator.ui.UserOutput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class VendingMachine {
     private UserOutput userOutput;
     private UserInput userInput;
+    private List<Food> listFood = new ArrayList<>();
+
 
     public VendingMachine(){
         this.userInput = new UserInput();
@@ -14,6 +24,9 @@ public class VendingMachine {
 
     public void run()
     {
+
+        readInFromFile();
+
         while(true)
         {
             userOutput.displayHomeScreen();
@@ -21,7 +34,12 @@ public class VendingMachine {
 
             if(choice.equals("display"))
             {
-                // display the vending machine slots
+                listFood.forEach(food -> {
+
+                    System.out.printf("%s %s $%s Quantity: %s \n", food.getItemLocation(), food.getName(), food.getPrice(), food.getQuantity() == 0 ? "NO LONGER AVAILABLE" : food.getQuantity());
+
+                        });
+
             }
             else if(choice.equals("purchase"))
             {
@@ -29,10 +47,51 @@ public class VendingMachine {
             }
             else if(choice.equals("exit"))
             {
-                // good byee
+                // good bye
                 break;
             }
         }
     }
-    
+
+    private void readInFromFile() {
+        File vendingFile = new File("catering.csv");
+
+        //logger later
+
+        try (Scanner fileScanner = new Scanner(vendingFile)) {
+            while (fileScanner.hasNextLine()) {
+
+                String line = fileScanner.nextLine();
+                String[] foodProperties = line.split(",");
+
+                String location = foodProperties[0];
+                String name = foodProperties[1];
+                BigDecimal price = new BigDecimal(foodProperties[2]);
+
+                switch (foodProperties[3].toUpperCase()) {
+                    case "CANDY":
+                        listFood.add(new Candy(location, name, price));
+                        break;
+                    case "GUM":
+                        listFood.add(new Gum(location, name, price));
+                        break;
+                    case "DRINK":
+                        listFood.add(new Drink(location, name, price));
+                        break;
+                    default:
+                        listFood.add(new Munchy(location, name, price));
+                        break;
+                }
+
+            }
+
+//            listFood.forEach(food -> System.out.println(food.getName()));
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found, ending program");
+            System.exit(0);
+        }
+
+    }
+
 }
