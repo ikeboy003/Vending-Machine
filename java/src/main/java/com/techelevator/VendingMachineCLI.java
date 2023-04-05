39f247fc69e8ea0d67a5d002e4ca79463d153e09
Nathanael Aninweze <nathanael.aninweze@outlook.com>
2023-04-05 12:58:07 -0400

    Sorry for missing this commit

--- java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,13 +1,16 @@
 package com.techelevator;
 
 import com.techelevator.application.VendingMachine;
-
+/*
+Need to add userselection for file
+ */
 public class VendingMachineCLI 
 {
     public static void main(String[] args) 
     {
         VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.run();
+
     }
     
 }
